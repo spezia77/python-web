e51c862365bd647aa632380e78ca0a3842694118
luis spz <lhspezia@gmail>
2024-10-09 22:08:25 -0300

    Django

---
+++ sistema/urls.py
@@ -0,0 +1,16 @@
+# urlpatterns é utilizado para definir quais serão as rotas
+# rota nada mais é do que o caminho para poder chegar a uma função
+from django.urls import path
+
+from sistema import views
+
+
+urlpatterns = [
+    # quando o usuário acessar o /home será executado a função
+    # index do arquivo sistema/views.py
+    path("/home", views.index),
+    path("/", views.index),
+    path("/contato", views.contato),
+    path("/calculadora", views.calculadora),
+    path("/calcular", views.calcular),
+]
